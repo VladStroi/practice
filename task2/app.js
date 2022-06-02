7928e673be37a57f275b7e523c75272bbd87a9ca
VladStroi <vladislavstroy@gmail.com>
2022-06-02 22:54:03 +0300

    edit app

--- task2/app.js
+++ task2/app.js
@@ -1,14 +1,27 @@
 const accardion = document.querySelector('.accardion');
-const part = document.querySelectorAll('.part')
+const title = document.querySelectorAll('.title')
 
-console.log(part);
 
-const tapTitle = () => {
-    part.forEach((el) => {
-        console.log(el.querySelector('.text'));
-        console.log(el.querySelector('.title'));
+// const tapTitle = (event) => {
+//     if (event.target.nextElementSibling.className == 'text active'){
+//         event.target.nextElementSibling.classList.remove('active')
+//     } else {
+//         event.target.nextElementSibling.classList.add('active')
+//     }
+//     console.log(event.target.nextElementSibling.className);
+// }
 
+title.forEach(btn => {
+    btn.addEventListener('click', () => {
+        console.log(btn.nextElementSibling.className);
+        if (btn.nextElementSibling.className == 'text active'){
+            btn.nextElementSibling.style.transition = '0.5s';
+            btn.nextElementSibling.classList.remove('active')
+        } else {
+            btn.nextElementSibling.style.transition = '0.5s';
+            btn.nextElementSibling.classList.add('active')
+        };
     })
-}
+})
 
-tapTitle()
+title.ch
