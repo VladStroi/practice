9be180a99ba2599a45978ad6c3c7d3fa2d0203cb
VladStroi <vladislavstroy@gmail.com>
2022-06-07 18:13:10 +0300

    test edit

--- task3/app.js
+++ task3/app.js
@@ -7,6 +7,12 @@ input.addEventListener('keydown', function enter(event) {
         let newText = document.createTextNode(input.value)
         newDiv.appendChild(newText)
         document.body.appendChild(newDiv)
+        let newToDo = document.getElementsByClassName('newToDo')
+        console.log(newToDo);
+        let checkbox = document.createElement("input")
+        console.log(checkbox);
+        // newToDo.insertBefore(checkbox, newToDo.firstChild)
+        // checkbox.type ="checkbox"
         input.value = ''
     }
 })
