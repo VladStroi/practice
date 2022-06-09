b95eef5b91da587641324c907e3a9e5dcdcbbaf9
VladStroi <vladislavstroy@gmail.com>
2022-06-09 16:19:19 +0300

    new edit

--- task3/app.js
+++ task3/app.js
@@ -1,18 +1,42 @@
 const input = document.querySelector('.input');
+const check = document.querySelector('.check')
 
 input.addEventListener('keydown', function enter(event) {
     if (event.keyCode === 13){
-        let newDiv = document.createElement('div')
-        newDiv.classList.add('newToDo')
-        let newText = document.createTextNode(input.value)
-        newDiv.appendChild(newText)
-        document.body.appendChild(newDiv)
-        let newToDo = document.getElementsByClassName('newToDo')
-        console.log(newToDo);
-        let checkbox = document.createElement("input")
-        console.log(checkbox);
-        // newToDo.insertBefore(checkbox, newToDo.firstChild)
-        // checkbox.type ="checkbox"
-        input.value = ''
+        //new div TO DO
+        const newDiv = document.createElement('div');
+        newDiv.classList.add('newToDo');
+        check.appendChild(newDiv);
+
+        //new value input add in TO DO
+        let newToDo = document.querySelectorAll('.newToDo');
+        let newText = document.createTextNode(input.value);
+        
+        //new checkbox
+        let checkbox = document.createElement('input')
+        checkbox.type = 'checkbox';
+        checkbox.classList.add('check')
+
+        //add value input
+        newToDo[newToDo.length - 1].appendChild(checkbox);
+        newDiv.appendChild(newText);
+
+        //add clean button
+        let addCleanButton = document.createElement('div');
+        addCleanButton.classList.add('clean');
+        let cleanX = document.createTextNode('×')
+        addCleanButton.appendChild(cleanX)
+        newToDo[newToDo.length - 1].appendChild(addCleanButton);
+        
+        
+
+        //clean value input
+        input.value = '';
     }
 })
+
+function deleted(e) {
+    if (e.target.className === 'check');
+}
+
+check.addEventListener('click', deleted)
